cede71fd7fae6930889d579257544d3fb27da592
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-02-27 17:49:09 +0530

    wishlist css update

--- adidas_app/src/components/ProductWishlist/ProductWishlist.jsx
+++ adidas_app/src/components/ProductWishlist/ProductWishlist.jsx
@@ -36,51 +36,63 @@ export const ProductWishlist = () => {
 
     return (
       <>
-      <Navbar />
-      <div id="pro_container_wrap">
-        
-        <div className="nac_wish_left">
-          <h1 className="nac_wishhead">MY WISHLIST</h1>
-          {/* <p>0 ITEMS</p>
+        <Navbar />
+        <div id="pro_container_wrap">
+          <div className="nac_wish_left">
+            <h1 className="nac_wishhead">MY WISHLIST</h1>
+            {/* <p>0 ITEMS</p>
           <p>
             You haven't saved any items to your wishlist yet. Start shopping and
             add your favorite items to your wishlist.
           </p> */}
-          <div className="nac_wishbox">
-              <img src={ret} alt=""/>
+            <div className="nac_wishbox">
+              <img src={ret} alt="" />
               <p className="nac_wisht">{res.title}</p>
-              <p>Rs. {res.price}</p>
+              <p id="wishlist_price">Rs. {res.price}</p>
+            </div>
+          </div>
+          <div className="nac_wish_right">
+            <h2 className="nac_wishhead">NEED HELP?</h2>
+            <ul className="nac_wright_a">
+              <li>
+                <a href="/Products" className="nac_black">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="" className="nac_black">
+                  Ordering & Payments
+                </a>
+              </li>
+              <li>
+                <a href="" className="nac_black">
+                  Delivery
+                </a>
+              </li>
+              <li>
+                <a href="" className="nac_black">
+                  Promotions & Vouchers
+                </a>
+              </li>
+              <li>
+                <a href="" className="nac_black">
+                  Return & Refund
+                </a>
+              </li>
+              <li>
+                <a href="" className="nac_black">
+                  Account & Newsletter
+                </a>
+              </li>
+              <li>
+                <a href="" className="nac_black">
+                  Company information
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="nac_wish_right">
-          <h2 className="nac_wishhead">NEED HELP?</h2>
-          <ul className="nac_wright_a">
-            <li>
-              <a href="" className="nac_black">Products</a>
-            </li>
-            <li>
-              <a href="" className="nac_black">Ordering & Payments</a>
-            </li>
-            <li>
-              <a href="" className="nac_black">Delivery</a>
-            </li>
-            <li>
-              <a href="" className="nac_black">Promotions & Vouchers</a>
-            </li>
-            <li>
-              <a href="" className="nac_black">Return & Refund</a>
-            </li>
-            <li>
-              <a href="" className="nac_black">Account & Newsletter</a>
-            </li>
-            <li>
-              <a href="" className="nac_black">Company information</a>
-            </li>
-          </ul>
-        </div>
-        
-      </div>
-      {/* // <Footer/> */}
+        <Footer />
       </>
     );
 }
